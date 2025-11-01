a8ded6129ca025c668ad0db73c204549b9096e28
Rob Masson <rmassonster@gmail.com>
2025-11-01 10:58:52 -0700

    added back filenames as arguments to refactored functions; added unit test file

--- penguins_refactored.py
+++ penguins_refactored.py
@@ -13,22 +13,22 @@ DATA_FILE_PATH = 'penguins_data.csv'
 
 
 
-def download_data():
+def download_data(data_file):
     # download the dataset if it doesn't exist already
     try:
-        with open(DATA_FILE_PATH, 'rb') as file:
+        with open(data_file, 'rb') as file:
             return pd.read_csv(file)
     except FileNotFoundError:
         url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/penguins.csv'
         response = requests.get(url)
-        with open(DATA_FILE_PATH, 'wb') as file:
+        with open(data_file, 'wb') as file:
             file.write(response.content)
 
 
-def clean_data():
+def clean_data(data_file):
     # drop rows with missing values
     # return numpy arrays for features and labels
-    df = pd.read_csv(DATA_FILE_PATH)
+    df = pd.read_csv(data_file)
     df = df.dropna(subset=['species', 'bill_length_mm', 'bill_depth_mm', 
                            'flipper_length_mm', 'body_mass_g'])
     features = df[['bill_length_mm', 'bill_depth_mm', 
@@ -38,60 +38,60 @@ def clean_data():
 
 
 
-def preprocess_data(features, labels):
+def preprocess_data(features, labels, scaler_file, encoder_file):
     # encode categorical variables
     # scale numerical features
     # split the data into train/test features and labels
     scaler = StandardScaler()
     features_scaled = scaler.fit_transform(features)
-    joblib.dump(scaler, SCALER_FILENAME)
+    joblib.dump(scaler, scaler_file)
     encoder = LabelEncoder()
     labels_encoded = encoder.fit_transform(labels)
-    joblib.dump(encoder, ENCODER_FILENAME)
+    joblib.dump(encoder, encoder_file)
     X_train, X_test, y_train, y_test = train_test_split(features_scaled, labels_encoded, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
 
 
-def train_model(X_train, y_train, X_test, y_test):
+def train_model(X_train, y_train, X_test, y_test, model_file):
     # train a model and save it
     clf = LogisticRegression() 
     clf.fit(X_train, y_train)
     # print the model's accuracy
     accuracy = clf.score(X_test, y_test)
     print(f'Model accuracy: {accuracy:.2f}')
-    joblib.dump(clf, MODEL_FILENAME)
+    joblib.dump(clf, model_file)
     
 
-def predict_new_data(X_new):
+def predict_new_data(X_new, model_file, scaler_file, encoder_file):
     # load the model and make predictions
     # return a string of the predicted class
-    clf = joblib.load(MODEL_FILENAME)
-    scaler = joblib.load(SCALER_FILENAME)
+    clf = joblib.load(model_file)
+    scaler = joblib.load(scaler_file)
     scaled_data = scaler.transform(X_new)
     prediction = clf.predict(scaled_data)
     prediction_prob = clf.predict_proba(scaled_data)[0][prediction[0]]
     # decode the prediction
-    encoder = joblib.load(ENCODER_FILENAME)
+    encoder = joblib.load(encoder_file)
     predicted_class = encoder.inverse_transform(prediction)
     return [predicted_class[0], prediction_prob]
 
 
 def run_training_pipeline():
     # load data
-    download_data()
+    download_data(DATA_FILE_PATH)
     
     # clean data
-    features, labels = clean_data()
+    features, labels = clean_data(DATA_FILE_PATH)
     
     # preprocess data
-    X_train, X_test, y_train, y_test = preprocess_data(features, labels)
+    X_train, X_test, y_train, y_test = preprocess_data(features, labels, SCALER_FILENAME, ENCODER_FILENAME)
     
     # train model
-    train_model(X_train, y_train, X_test, y_test)
+    train_model(X_train, y_train, X_test, y_test, MODEL_FILENAME)
 
 
 if __name__ == "__main__":
     run_training_pipeline()
-    print(predict_new_data([[40, 17, 190, 3500]]))
-    print(predict_new_data([[80, 10, 250, 2500]]))
+    print(predict_new_data([[40, 17, 190, 3500]], MODEL_FILENAME, SCALER_FILENAME, ENCODER_FILENAME))
+    print(predict_new_data([[80, 10, 250, 2500]], MODEL_FILENAME, SCALER_FILENAME, ENCODER_FILENAME))
